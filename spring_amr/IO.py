b7cee443099b1316502bebc58857d9f571cdaba0
jcyk <thisisjcykcd@gmail.com>
2022-03-18 21:58:32 +0800

    snt_lang

--- spring_amr/IO.py
+++ spring_amr/IO.py
@@ -17,6 +17,7 @@ def read_raw_amr_data(
     graphs = []
     for path_ in paths:
         for path in glob.glob(str(path_)):
+            unkown_lang = set()
             path = Path(path)
             #en_XX, de_DE, es_XX, it_IT, zh_CN
             if str(path).endswith("_de.txt"):
@@ -31,10 +32,25 @@ def read_raw_amr_data(
                 lang_code = "en_XX"
             pm_graphs = pm_load(path, dereify=dereify, remove_wiki=remove_wiki)
             for g in pm_graphs:
-                g.metadata["snt_lang"] = lang_code
+                if ("snt_lang" not in g.metadata) or (g.metadata["snt_lang"] not in ['zh', 'it', 'es', 'de', 'en']):
+                    unkown_lang.add(g.metadata["snt_lang"] if "snt_lang" in g.metadata else 'empty')
+                    g.metadata["snt_lang"] = lang_code
+                if g.metadata["snt_lang"] == 'zh':
+                    g.metadata["snt_lang"] = 'zh_CN'
+                elif g.metadata["snt_lang"] == 'it':
+                    g.metadata["snt_lang"] = 'it_IT'
+                elif g.metadata["snt_lang"] == 'es':
+                    g.metadata["snt_lang"] = 'es_XX'
+                elif g.metadata["snt_lang"] == 'de':
+                    g.metadata["snt_lang"] = 'de_DE'
+                elif g.metadata["snt_lang"] == 'en':
+                    g.metadata["snt_lang"] = 'en_XX'
+
                 if lang_code == "zh_CN":
                     g.metadata["snt"] = ''.join(g.metadata["snt"].split())
             graphs.extend(pm_graphs)
+            if len(unkown_lang) > 0:
+                print (f"unkown input language {unkown_lang} in {path}, treat it as {lang_code}")
 
     #assert graphs
     
